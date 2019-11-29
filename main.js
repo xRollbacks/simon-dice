6781971ba69c8a6bbad1f7080db6e32e9ee8ef1c
xRollbacks <56001809+xRollbacks@users.noreply.github.com>
2019-11-29 17:36:31 -0300

    Commit para comparar codigo actual con el codigo despues de la clase 10.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const $azul = document.querySelector("#azul")
 const $verde = document.querySelector("#verde")
 const $rojo = document.querySelector("#rojo")
@@ -41,3 +42,73 @@ $chequearColores.onclick = function chequearColores() {
         }
     }
 }
+=======
+const $azul = document.querySelector(".azul");
+const $verde = document.querySelector(".verde");
+const $rojo = document.querySelector(".rojo");
+const $amarillo = document.querySelector(".amarillo");
+const arrayColores = [];
+const $submit = document.querySelector("#submitColores");
+const $chequearColores = document.querySelector("#chequearColores");
+const $inputColores = document.querySelector("#inputColores");
+let arrayInputs = [];
+
+function devolverColorAlAzar() {
+	const numeroRandom = Math.random();
+	if (numeroRandom >= 0 && numeroRandom < 0.25) {
+		return "azul";
+	} else if (numeroRandom >= 0.25 && numeroRandom < 0.5) {
+		return "rojo";
+	} else if (numeroRandom >= 0.5 && numeroRandom < 0.75) {
+		return "verde";
+	} else if (numeroRandom >= 0.75 && numeroRandom <= 1) {
+		return "amarillo";
+	}
+}
+$azul.style.cursor = "pointer";
+$rojo.style.cursor = "pointer";
+$verde.style.cursor = "pointer";
+$amarillo.style.cursor = "pointer";
+
+function pushColorAlArray() {
+	arrayColores.push(devolverColorAlAzar());
+
+	console.log(arrayColores);
+}
+$submit.onclick = function() {
+	pushColorAlArray();
+	arrayInputs.length = 0;
+};
+
+$azul.onclick = function() {
+	arrayInputs.push("azul");
+	console.log(arrayInputs);
+};
+
+$rojo.onclick = function() {
+	arrayInputs.push("rojo");
+	console.log(arrayInputs);
+};
+
+$verde.onclick = function() {
+	arrayInputs.push("verde");
+	console.log(arrayInputs);
+};
+
+$amarillo.onclick = function() {
+	arrayInputs.push("amarillo");
+	console.log(arrayInputs);
+};
+
+$chequearColores.onclick = chequearColores;
+
+function chequearColores() {
+	for (let i = 0; arrayColores.length > i; i++) {
+		if (arrayInputs[i] === arrayColores[i]) {
+			console.log("Vas bien");
+		} else if (arrayInputs[i] !== arrayColores[i]) {
+			console.log(arrayInputs[i], arrayColores[i]);
+		}
+	}
+}
+>>>>>>> Stashed changes
